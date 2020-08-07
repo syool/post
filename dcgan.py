a5c139d8a57e388a80b2ccb6f07ccda2d94a5fa5
austin hyeon <57378337+austinet@users.noreply.github.com>
2020-08-07 15:45:11 +0900

    Create dcgan.py

---
+++ dcgan.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import matplotlib.pyplot as plt
+import glob
+import os
+import imageio
+import PIL
+import time
+
+from IPython import display
+
+# fetch dataset(MNIST)
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # Normalize images into [-1, 1]
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+# create batchs and suffle them
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+
+def make_generator():
+    model = tf.keras.Sequential()
+    # Fully connected
+    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    # Transition
+    model.add(layers.Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) # 주목: 배치사이즈로 None이 주어집니다.
+    # transpconv 1
+    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    # transpconv 2
+    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(layers.BatchNormalization())
+    model.add(layers.LeakyReLU())
+    # transpconv 3
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+
+    return model
+
+generator = make_generator()
+
+noise = tf.random.normal([1, 100])
+generated_image = generator(noise, training=False)
+
+plt.imshow(generated_image[0, :, :, 0], cmap='gray')
