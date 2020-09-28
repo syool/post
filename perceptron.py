3ddac805daae499845174fe5451f991d167a7504
austin hyeon <57378337+austinet@users.noreply.github.com>
2020-09-28 17:39:52 +0900

    Create perceptron.py

---
+++ perceptron.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+and_x = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]]) # AND Operation
+or_x = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]) # OR Operation
+
+w = np.random.randn(1, 2)
+b =  
+
+# print(w)
+# print(w[0, 0])
+# print(w[0, 1])
+
+# print(and_x[1, 1])
+
+y = and_x[i, j] * w[i, i]
