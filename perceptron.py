52ff5efba3c527a06c87900b24580c5deeb85dc9
austin hyeon <57378337+austinet@users.noreply.github.com>
2020-09-28 20:10:43 +0900

    으아아

--- perceptron.py
+++ perceptron.py
@@ -1,15 +1,40 @@
 import numpy as np
 
-and_x = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]]) # AND Operation
-or_x = np.array([[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]) # OR Operation
+# AND Operation
+and_op = np.array([[0, 0, 0],
+                   [0, 1, 0],
+                   [1, 0, 0],
+                   [1, 1, 1]])
+and_x = and_op[0:4, :-1] # input data
+and_Y = and_op[0:4, 2:] # labeled data
 
+# OR Operation
+or_op = np.array([[0, 0, 0],
+                  [0, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]])
+or_x = or_op[0:4, :-1] # input data
+or_Y = or_op[0:4, 2:] # labeled data
+
+# parameters
 w = np.random.randn(1, 2)
-b =  
+b = np.random.randn()
 
-# print(w)
-# print(w[0, 0])
-# print(w[0, 1])
+# activation function
+def step_function(x):
+    x = 0 if x <= 0 else 1
+    return x
 
-# print(and_x[1, 1])
+# perceptron
+epoch = 10
+tmp, y = [], []
+for repeat in range(epoch):
+    for i in range(len(and_op)):
+        p1 = and_x[i][0] * w[0][0]
+        p2 = and_x[i][1] * w[0][1]
+        tmp.append(p1 + p2 + b)
+        y.append(step_function(tmp[i]))
+        
+        print(step_function(tmp[i]))
 
-y = and_x[i, j] * w[i, i]
+print(and_Y)
---
+++ sketch.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([])
+
+np.hstack(a, np.array[1])
+
+print(a)
