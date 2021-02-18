0e5c9a8e77c5e6b9bf11e899c9895423e60d559c
syool <milim0708@outlook.com>
2021-02-18 13:29:43 +0900

    fix

--- gan.py
+++ gan.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 # import torch.backends.cudnn as cudnn
 
-os.makedirs("/home/austin/Documents/Outputs/gan", exist_ok=True)
+os.makedirs("/home/user/Documents/Outputs/gan", exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
@@ -38,10 +38,10 @@ print(cuda)
 # cudnn.benchmark = True
 
 # Configure data loader
-# os.makedirs("/home/austin/Documents/Datasets/", exist_ok=True)
+os.makedirs("/home/user/Documents/Datasets/", exist_ok=True)
 dataloader = torch.utils.data.DataLoader(
     datasets.MNIST(
-        "/home/austin/Documents/Datasets/",
+        "/home/user/Documents/Datasets/",
         train=True,
         download=True,
         transform=transforms.Compose(
@@ -171,4 +171,4 @@ for epoch in range(opt.n_epochs):
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "/home/austin/Documents/Outputs/gan/%d.png" % batches_done, nrow=5, normalize=True)
+            save_image(gen_imgs.data[:25], "/home/user/Documents/Outputs/gan/%d.png" % batches_done, nrow=5, normalize=True)
